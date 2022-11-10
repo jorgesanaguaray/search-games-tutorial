274fc82efa3b89e736cef9d086b423698393d606
Jorge Sanaguaray <jorgesanaguaray1@gmail.com>
2022-11-10 10:30:31 -0500

    I added the Application class to the Android Manifest, in addition to adding the internet permission.

---
+++ app/src/main/java/com/jorgesanaguaray/searchgamestutorial/app/SearchGamesTutorial.kt
@@ -0,0 +1,11 @@
+package com.jorgesanaguaray.searchgamestutorial.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Jorge Sanaguaray
+ */
+
+@HiltAndroidApp
+class SearchGamesTutorial : Application()
